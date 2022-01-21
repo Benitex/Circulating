4a8c070e93718fe24406a3ef0d5b36115181131c
Benitex <benitoapepe@yahoo.com.br>
2022-01-20 22:13:59 -0300

    New mouse sprite for menus

--- Mouse.lua
+++ Mouse.lua
@@ -1,11 +1,17 @@
 local Mouse = {}
-Mouse.sprite = love.graphics.newImage('graphics/Mouse.png')
+Mouse.sprites = {}
+Mouse.sprites.player = love.graphics.newImage('graphics/mouse/Player.png')
+Mouse.sprites.menus = love.graphics.newImage('graphics/mouse/Mouse.png')
 Mouse.width = 32
 Mouse.height = 32
 Mouse.scale = 1
 
 function Mouse.draw()
-    love.graphics.draw(Mouse.sprite, love.mouse.getX() - Mouse.width*Mouse.scale/2, love.mouse.getY() - Mouse.height*Mouse.scale/2, 0, Mouse.scale, Mouse.scale)
+    if gameState == 'play' then
+        love.graphics.draw(Mouse.sprites.player, love.mouse.getX() - Mouse.width*Mouse.scale/2, love.mouse.getY() - Mouse.height*Mouse.scale/2, 0, Mouse.scale, Mouse.scale)
+    else
+        love.graphics.draw(Mouse.sprites.menus, love.mouse.getX() - Mouse.width*Mouse.scale/2, love.mouse.getY() - Mouse.height*Mouse.scale/2, 0, Mouse.scale, Mouse.scale)
+    end
 end
 
 return Mouse
